27b436d451d0f0c8f99293eb4ae47861f570ff39
Aria Espahbodi <ariaes10@gmail.com>
2021-06-01 23:36:31 +0430

    class Maze developed!

--- cpp/maze.cpp
+++ cpp/maze.cpp
@@ -1,2 +1,15 @@
 #include <Maze.h>
+#include <math.h>
 
+Maze::Maze(double N) {
+    if (N<9)
+        throw std::logic_error("too little dimensions!");
+    double branchingtimes{round(sqrt(N))};
+    double counter{};    
+    //while()
+
+}
+
+std::list<Maze::Node*> Maze::branching() {
+
+}
--- h/maze.h
+++ h/maze.h
@@ -5,6 +5,45 @@
 #include <list>
 #include <set>
 
+class Maze {
+private:
+    size_t N{};
+    class Node {
+    public:
+        double value{0};
+        Node* parent{nullptr};
+        std::list<Node*> children;
+        Node()=default;
+        Node(double);
+        explicit Node(double, Node&, std::list<Node*>);
+        //Node(const Node& n, BinomialHeap& outBH );
+        size_t get_order() const;
+        void set_order(size_t new_order) {order=new_order;};
+
+        std::partial_ordering operator<=> (const Node& n1) {return this->value <=> n1.value;}
+        bool operator== (const Node& n1) {return this->value == n1.value;}
+        
+        friend std::ostream &operator<< (std::ostream &os, Node const& n1) {
+            if (n1.parent==nullptr)
+                return os << "(value:" << n1.value << ", parent: " << ", order:" << n1.order<< ")" << std::endl;
+            else 
+                return os << "(value:" << n1.value << ", parent:" << n1.parent->value << ", order:" << n1.order<< ")" << std::endl;
+        }
+
+        static Node* merge(Node* , Node* );
+        static bool is_min_heap(Node const&);
+        static bool is_max_heap(Node const&);
+    private:
+        size_t order{0};  
+    };
+public:
+    Maze()=default;
+    Maze(double);
+    ~Maze();
+    Maze(const Maze&);
+    std::list<Node*> branching();
+
+};
 
 
 
